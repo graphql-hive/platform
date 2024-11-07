ed71480af827043a054f961106c7ff6b12253eb0
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-11-07 22:04:39 +0300

    add target_id as unique

--- packages/migrations/src/actions/2024.11.07T00.00.00.create-preflight-scripts.ts
+++ packages/migrations/src/actions/2024.11.07T00.00.00.create-preflight-scripts.ts
@@ -1,22 +1,23 @@
 import { type MigrationExecutor } from '../pg-migrator';
 
 export default {
-  name: '2024.09.26T00.00.00.create-preflight-scripts.sql',
+  name: '2024.11.07T00.00.00.create-preflight-scripts.sql',
   run: ({ sql }) => sql`
 CREATE TABLE "document_preflight_scripts" (
   "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
   "source_code" text NOT NULL,
-  "target_id" uuid NOT NULL REFERENCES "targets"("id") ON DELETE CASCADE,
+  "target_id" uuid NOT NULL UNIQUE REFERENCES "targets"("id") ON DELETE CASCADE,
   "created_by_user_id" uuid REFERENCES "users"("id") ON DELETE SET NULL,
   "created_at" timestamptz NOT NULL DEFAULT now(),
   "updated_at" timestamptz NOT NULL DEFAULT now(),
   PRIMARY KEY ("id")
 );
 
-CREATE INDEX "document_preflight_scripts_connection_pagination" ON "document_preflight_scripts" (
-  "target_id" ASC,
-  "created_at" DESC,
-  "id" DESC
+ALTER TABLE "document_preflight_scripts"
+ADD CONSTRAINT "unique_target_id" UNIQUE ("target_id");
+
+CREATE INDEX "document_preflight_scripts_target" ON "document_preflight_scripts" (
+  "target_id" ASC
 );
 `,
 } satisfies MigrationExecutor;
