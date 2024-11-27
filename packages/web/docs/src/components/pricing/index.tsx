4e8a3ffb982a24407e48ebb2b079b37a94f97637
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2024-11-27 14:24:23 +0100

    Move PricingSlider to client component file

--- packages/web/docs/src/components/pricing/index.tsx
+++ packages/web/docs/src/components/pricing/index.tsx
@@ -1,7 +1,7 @@
-import { HTMLAttributes, ReactElement, ReactNode, useState } from 'react';
+import { HTMLAttributes, ReactElement, ReactNode } from 'react';
 import { Arrow, Content, Root, Trigger } from '@radix-ui/react-tooltip';
 import { CallToAction, cn } from '@theguild/components';
-import { Slider } from './slider';
+import { PricingSlider } from './pricing-slider';
 
 function Tooltip({ content, children }: { content: string; children: ReactNode }) {
   return (
@@ -198,48 +198,5 @@ export function Pricing({
         </div>
       </div>
     </section>
-  );
-}
-
-function PricingSlider({ className, ...rest }: { className?: string }) {
-  const min = 1;
-  const max = 300;
-
-  const [millionsOfOperations, setMillionsOfOperations] = useState(min);
-
-  return (
-    <label className={cn(className, 'block')} {...rest}>
-      <div className="text-green-1000 font-medium">Expected monthly operations?</div>
-      <div className="text-green-1000 flex items-center gap-2 pt-12 text-sm">
-        <span className="font-medium">{min}M</span>
-        <Slider
-          min={min}
-          max={max}
-          defaultValue={min}
-          // 10$ base price + 10$ per 1M
-          style={{ '--ops': min, '--price': 'calc(10 + var(--ops) * 10)' }}
-          counter="after:content-[''_counter(ops)_'M_operations,_$'_counter(price)_'_/_month'] after:[counter-set:ops_calc(var(--ops))_price_calc(var(--price))]"
-          onChange={event => {
-            const value = event.currentTarget.valueAsNumber;
-            setMillionsOfOperations(value);
-            event.currentTarget.parentElement!.style.setProperty('--ops', `${value}`);
-          }}
-        />
-        <span className="font-medium">{max}M</span>
-      </div>
-      <p
-        className="mt-4 rounded-xl bg-green-100 p-3 transition"
-        style={{ opacity: millionsOfOperations >= max * 0.95 ? 1 : 0 }}
-      >
-        <span className="font-medium">Running {max}M+ operations?</span>
-        <br />
-        <button
-          className="underline hover:text-blue-700"
-          onClick={() => (window as any).$crisp?.push(['do', 'chat:open'])}
-        >
-          Talk to us
-        </button>
-      </p>
-    </label>
   );
 }
---
+++ packages/web/docs/src/components/pricing/pricing-slider.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import { useState } from 'react';
+import { cn } from '@theguild/components';
+import { Slider } from '../slider';
+
+export function PricingSlider({ className, ...rest }: { className?: string }) {
+  const min = 1;
+  const max = 300;
+
+  const [millionsOfOperations, setMillionsOfOperations] = useState(min);
+
+  return (
+    <label className={cn(className, 'block')} {...rest}>
+      <div className="text-green-1000 font-medium">Expected monthly operations?</div>
+      <div className="text-green-1000 flex items-center gap-2 pt-12 text-sm">
+        <span className="font-medium">{min}M</span>
+        <Slider
+          min={min}
+          max={max}
+          defaultValue={min}
+          // 10$ base price + 10$ per 1M
+          style={{ '--ops': min, '--price': 'calc(10 + var(--ops) * 10)' }}
+          counter="after:content-[''_counter(ops)_'M_operations,_$'_counter(price)_'_/_month'] after:[counter-set:ops_calc(var(--ops))_price_calc(var(--price))]"
+          onChange={event => {
+            const value = event.currentTarget.valueAsNumber;
+            setMillionsOfOperations(value);
+            event.currentTarget.parentElement!.style.setProperty('--ops', `${value}`);
+          }}
+        />
+        <span className="font-medium">{max}M</span>
+      </div>
+      <p
+        className="mt-4 rounded-xl bg-green-100 p-3 transition"
+        style={{ opacity: millionsOfOperations >= max * 0.95 ? 1 : 0 }}
+      >
+        <span className="font-medium">Running {max}M+ operations?</span>
+        <br />
+        <button
+          className="underline hover:text-blue-700"
+          onClick={() => (window as any).$crisp?.push(['do', 'chat:open'])}
+        >
+          Talk to us
+        </button>
+      </p>
+    </label>
+  );
+}
