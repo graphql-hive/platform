293d29abf10b1a6b853acd0c4f14dc9ee2900973
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2024-11-29 17:41:13 +0100

    Move TextLink style to MDX a

--- packages/web/docs/mdx-components.js
+++ packages/web/docs/mdx-components.js
@@ -1,8 +1,6 @@
 import { useMDXComponents as getDocsMDXComponents } from '@theguild/components/server';
 import { docsMDXComponents } from './src/components/docs-mdx-components';
 
-// TODO: Overwrite `a` to fix text-underline-position: from-font in Safari.
-
 export const useMDXComponents = () => {
   return getDocsMDXComponents(docsMDXComponents);
 };
--- packages/web/docs/src/components/docs-mdx-components.tsx
+++ packages/web/docs/src/components/docs-mdx-components.tsx
@@ -1,16 +1,15 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Anchor } from 'nextra/components';
 import { cn } from '@theguild/components';
-import { useMDXComponents as getDocsMDXComponents } from '@theguild/components/server';
 
-/**
- * @type {import('nextra/components').Anchor}
- */
 export const Link: typeof Anchor = ({ className, ...props }) => {
   return (
     <Anchor
       // we remove `text-underline-position` from default Nextra link styles
-      className={cn('text-blue-700 underline hover:no-underline dark:text-blue-500', className)}
+      className={cn(
+        'hive-focus -mx-1 -my-0.5 rounded px-1 py-0.5 text-blue-700 underline hover:no-underline focus-visible:ring-current focus-visible:ring-offset-blue-200 dark:text-blue-500 dark:focus-visible:ring-offset-blue-800',
+        className,
+      )}
       {...props}
     />
   );
