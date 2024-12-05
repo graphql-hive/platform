319d1a3213af5640491492bf0e0e4fb749e6fea8
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2024-12-05 11:35:16 +0100

    Specify developerMenu explicitly

--- packages/web/docs/src/components/navigation-menu/index.tsx
+++ packages/web/docs/src/components/navigation-menu/index.tsx
@@ -1,8 +1,37 @@
 'use client';
 
 import { usePathname } from 'next/navigation';
-import { GraphQLConfCard, HiveNavigation, PRODUCTS } from '@theguild/components';
+import {
+  GitHubIcon,
+  GraphQLConfCard,
+  HiveNavigation,
+  PaperIcon,
+  PencilIcon,
+  PRODUCTS,
+  RightCornerIcon,
+  TargetIcon,
+} from '@theguild/components';
 import graphQLConfLocalImage from './graphql-conf-image.webp';
+
+const developerMenu = [
+  {
+    href: '/docs',
+    icon: PaperIcon,
+    children: 'Documentation',
+  },
+  { href: 'https://status.graphql-hive.com/', icon: TargetIcon, children: 'Status' },
+  {
+    href: '/product-updates',
+    icon: RightCornerIcon,
+    children: 'Product Updates',
+  },
+  { href: '/blog', icon: PencilIcon, children: 'Blog' },
+  {
+    href: 'https://github.com/dotansimha/graphql-code-generator',
+    icon: GitHubIcon,
+    children: 'GitHub',
+  },
+];
 
 export function NavigationMenu() {
   const route = usePathname();
@@ -12,6 +41,7 @@ export function NavigationMenu() {
       className={isLandingPage(route) ? 'light max-w-[1392px]' : 'max-w-[90rem]'}
       companyMenuChildren={<GraphQLConfCard image={graphQLConfLocalImage} />}
       productName={PRODUCTS.HIVE.name}
+      developerMenu={developerMenu}
     />
   );
 }
