b0baa6dca0c7344ffd6431acedae97b18911ba60
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2024-12-06 12:58:58 +0100

    Remove unused ts-expect-error

--- packages/web/docs/tailwind.config.ts
+++ packages/web/docs/tailwind.config.ts
@@ -3,7 +3,6 @@ import type { Config } from 'tailwindcss';
 import tailwindcssAnimate from 'tailwindcss-animate';
 import tailwindcssRadix from 'tailwindcss-radix';
 import { fontFamily } from 'tailwindcss/defaultTheme';
-// @ts-expect-error -- types are missing
 import { default as flattenColorPalette } from 'tailwindcss/lib/util/flattenColorPalette';
 import plugin from 'tailwindcss/plugin';
 import type { PluginAPI } from 'tailwindcss/types/config';
