407cd016f979ba92eedffc6987287b9785fedeca
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2024-12-10 19:24:11 +0100

    Update packages/web/docs/src/app/dynamic-meta-tags.tsx
    Co-authored-by: Dimitri POSTOLOV <dmytropostolov@gmail.com>

--- packages/web/docs/src/app/dynamic-meta-tags.tsx
+++ packages/web/docs/src/app/dynamic-meta-tags.tsx
@@ -73,7 +73,6 @@ export function DynamicMetaTags() {
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
       <meta property="og:locale" content="en_US" />
-      <meta property="og:description" content={description} />
       <meta property="og:url" content={canonicalUrl} />
       <meta property="og:image" content={ensureAbsolute(ogImage ?? '/og-image.png')} />
       <meta property="og:image:alt" content={description} />
