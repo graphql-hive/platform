94dede1b015e16afb2630e22a63dd78a863fb6d4
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-12-11 05:27:36 +0700

    more

--- packages/web/docs/mdx-components.js
+++ packages/web/docs/mdx-components.js
@@ -1,8 +1,8 @@
 /**
- * We have an error while using `mdx-components` with `.tsx` extension
+ * We have an error while using `mdx-components` with `.tsx` extension:
  *
  * createContext only works in Client Components. Add the "use client" directive at the top of the file to use it
  *
- * Using `.jsx` to avoid this error for now
+ * Using `.js` to avoid this error for now
  */
 export { useMDXComponents } from './src/mdx-components';
