dcff1943137cc88ab205d0be10b22b190d184722
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-12-11 04:41:44 +0700

    more

--- packages/web/docs/src/app/feed.xml/route.ts
+++ packages/web/docs/src/app/feed.xml/route.ts
@@ -1,6 +1,19 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import RSS from 'rss';
 import { getChangelogs } from '../../components/product-updates';
+
+// Generate a TypeScript file with the latest changelogs
+const outputFilePath = path.resolve(
+  '..',
+  'app',
+  'src',
+  'components',
+  'ui',
+  'changelog',
+  'generated-changelog.ts',
+);
 
 export async function GET() {
   const feed = new RSS({
@@ -9,7 +22,9 @@ export async function GET() {
     feed_url: 'https://the-guild.dev/graphql/hive/feed.xml',
   });
 
-  for (const item of await getChangelogs()) {
+  const changelogs = await getChangelogs();
+
+  for (const item of changelogs) {
     feed.item({
       title: item.title,
       date: item.date,
@@ -17,6 +32,24 @@ export async function GET() {
       description: item.description,
     });
   }
+  // Sort changelogs by date and get the latest 4 records
+  const latestChangelog = [...changelogs]
+    .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+    .slice(0, 4)
+    .map(item => ({
+      date: new Date(item.date).toLocaleDateString('en', {
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric',
+      }),
+      href: `https://the-guild.dev/graphql/hive${item.route}`,
+      title: item.title,
+      description: item.description,
+    }));
+
+  const outputContent = `export const latestChangelog = ${JSON.stringify(latestChangelog, null, 2)};\n`;
+  await fs.writeFile(outputFilePath, outputContent, 'utf8');
+  console.info(`Generated successfully at: ${path.relative(process.cwd(), outputFilePath)}`);
 
   return new Response(feed.xml({ indent: true }), {
     headers: {
