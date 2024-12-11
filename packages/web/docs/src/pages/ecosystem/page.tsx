ade2016f7f86dbfc5e85ac9f0415365adb17df25
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2024-12-11 16:02:54 +0100

    Move metadata to MDX file to work with Nextra 3

--- packages/web/docs/src/pages/ecosystem/page.tsx
+++ packages/web/docs/src/pages/ecosystem/page.tsx
@@ -3,11 +3,6 @@ import { Page as LandingPageContainer } from '../../components/page';
 import { components } from './components';
 import EcosystemPageContent from './content.mdx';
 
-export const metadata = {
-  title: 'The Ecosystem',
-  description: 'Everything you need to scale your API infrastructure',
-};
-
 export default function EcosystemPage() {
   return (
     <LandingPageContainer className="text-green-1000 light mx-auto max-w-[90rem] overflow-hidden [&>:not(header)]:px-4 lg:[&>:not(header)]:px-8 xl:[&>:not(header)]:px-[120px]">
